a77eb7a1438d7e1a268dd0ed22730cdb1617a1c0
Mario <mario.espada77@gmail.com>
2023-03-20 21:53:46 +0100

    Initialize server for run in local

--- cmd/bootstrap/bootstrap.go
+++ cmd/bootstrap/bootstrap.go
@@ -1,1 +1,36 @@
 package bootstrap
+
+import (
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/bypepe77/spotify-clone-go/internal/infrastructure/bootstrap"
+	"github.com/joho/godotenv"
+)
+
+const enviroment = "dev"
+const projectDirName = "spotify-clone-go"
+
+func Run() error {
+	if enviroment != "dev" {
+		loadEnv()
+	}
+
+	config := bootstrap.NewConfig(os.Getenv("APP_NAME"), os.Getenv("PORT"))
+	server := bootstrap.NewServer(config)
+
+	return server.Run()
+}
+
+func loadEnv() {
+	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	currentWorkDirectory, _ := os.Getwd()
+	rootPath := projectName.Find([]byte(currentWorkDirectory))
+
+	err := godotenv.Load(string(rootPath) + `/.env`)
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"log"
+
+	"github.com/bypepe77/spotify-clone-go/cmd/bootstrap"
+)
+
+func main() {
+	err := bootstrap.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
