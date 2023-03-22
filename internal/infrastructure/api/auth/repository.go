b16ba8d71e7187561fe0f089af37e3ba092408cd
Mario <mario.espada77@gmail.com>
2023-03-22 19:52:15 +0100

    create user repository

---
+++ internal/infrastructure/api/auth/repository.go
@@ -0,0 +1,57 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/bypepe77/spotify-clone-go/ent"
+	"github.com/bypepe77/spotify-clone-go/ent/user"
+)
+
+type authRepository struct {
+	db *ent.Client
+}
+
+type AuthRepositoryInterface interface {
+	GetUserByUsername(username string) (*ent.User, error)
+	GetUserByEmail(email string) (*ent.User, error)
+	GetUserById(id int) (*ent.User, error)
+	CreateUser(username string, email string, password string) (*ent.User, error)
+}
+
+func NewAuthRepository(db *ent.Client) AuthRepositoryInterface {
+	return &authRepository{
+		db: db,
+	}
+}
+
+func (r *authRepository) GetUserByUsername(username string) (*ent.User, error) {
+	user, err := r.db.User.Query().Where(user.Username(username)).Only(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (r *authRepository) GetUserByEmail(email string) (*ent.User, error) {
+	user, err := r.db.User.Query().Where(user.Email(email)).Only(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (r *authRepository) GetUserById(id int) (*ent.User, error) {
+	user, err := r.db.User.Query().Where(user.ID(id)).Only(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (r *authRepository) CreateUser(username string, email string, password string) (*ent.User, error) {
+	user, err := r.db.User.Create().SetUsername(username).SetEmail(email).Save(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
