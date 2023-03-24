94cf6333d7c39935ea22bfd7cfac39ad8484e113
Mario <mario.espada77@gmail.com>
2023-03-24 23:12:11 +0100

    Add register in domain/auth

--- internal/domain/auth/service.go
+++ internal/domain/auth/service.go
@@ -8,6 +8,7 @@ import (
 
 type AuthServiceInterface interface {
 	Login(username string, password string) (*User, error)
+	Register(username string, password string) (*User, error)
 }
 
 type AuthService struct {
@@ -23,7 +24,6 @@ func NewAuthService(repository AuthRepository) AuthServiceInterface {
 }
 
 func (s *AuthService) Login(username string, password string) (*User, error) {
-
 	if username == "" || password == "" {
 		return nil, errors.New("username or password is empty")
 	}
@@ -36,6 +36,28 @@ func (s *AuthService) Login(username string, password string) (*User, error) {
 	// TODO: Hash password
 	if user.Password != password || user.Username != username {
 		return nil, errors.New("invalid password or username")
+	}
+
+	token, err := s.jwtService.GenerateToken(user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return toUserModel(user.ID, user.Username, token), nil
+}
+
+func (s *AuthService) Register(username string, password string) (*User, error) {
+	if username == "" || password == "" {
+		return nil, errors.New("username or password is empty")
+	}
+
+	user, err := s.repository.GetUserByUsername(username)
+	if err != nil {
+		return nil, err
+	}
+
+	if user != nil {
+		return nil, errors.New("username already exists")
 	}
 
 	token, err := s.jwtService.GenerateToken(user.ID)
