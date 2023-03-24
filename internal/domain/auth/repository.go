bc76e57d29a5a8c986d475002fd8b10ebf9ca628
Mario <mario.espada77@gmail.com>
2023-03-24 22:36:43 +0100

    Add domain for auth with repository and service

--- internal/domain/auth/repository.go
+++ internal/domain/auth/repository.go
@@ -1,1 +1,10 @@
 package auth
+
+import "github.com/bypepe77/spotify-clone-go/ent"
+
+type AuthRepository interface {
+	GetUserByUsername(username string) (*ent.User, error)
+	GetUserByEmail(email string) (*ent.User, error)
+	GetUserById(id int) (*ent.User, error)
+	CreateUser(username string, email string, password string) (*ent.User, error)
+}
--- internal/domain/auth/service.go
+++ internal/domain/auth/service.go
@@ -1,16 +1,14 @@
 package auth
 
-import "github.com/bypepe77/spotify-clone-go/ent"
-
-type authService struct {
-	db *ent.Client
-}
-
 type AuthServiceInterface interface {
 }
 
-func NewAuthService(db *ent.Client) AuthServiceInterface {
-	return &authService{
-		db: db,
+type AuthService struct {
+	repository AuthRepository
+}
+
+func NewAuthService(repository AuthRepository) AuthServiceInterface {
+	return &AuthService{
+		repository: repository,
 	}
 }
